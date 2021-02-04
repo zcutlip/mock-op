4106a093c555b125201ab3189e4bf9b3769ff2a4
Zachary Cutlip <uid000@gmail.com>
2021-02-03 16:18:09 -0800

    refactored mock-op entrypoint
    - added MockOP class that can be extended
    - moved most of mock_op into MockOP class
    - added cli.py for main functions
    - mock-op entrypoint instantiates MockOP object

---
+++ mock_op/cli.py
@@ -0,0 +1,36 @@
+import sys
+import shlex
+
+from mock_cli import ResponseDirectory
+from mock_cli.argv_conversion import argv_from_string
+
+from .mock_op import MockOP
+
+
+def mock_op_main():
+    mock_op_cmd = MockOP()
+    # We parse args in order to fail on args we don't understand
+    # even though we don't actually use them
+    mock_op_cmd.parse_args()
+
+    args = sys.argv[1:]
+    exit_status = mock_op_cmd.respond(args)
+    return exit_status
+
+
+def list_invocations_main():
+    mock_op_cmd = MockOP()
+    resp_dir_path = mock_op_cmd.response_directory_path
+    directory = ResponseDirectory(resp_dir_path)
+
+    commands = directory.commands
+
+    for cmd in commands.keys():
+        cmd_args = argv_from_string(cmd)
+        arg_string = shlex.join(cmd_args)
+
+        print(f"{arg_string}")
+
+
+if __name__ == "__main__":
+    exit(mock_op_main())
--- mock_op/mock_op.py
+++ mock_op/mock_op.py
@@ -1,63 +1,69 @@
 #!/usr/bin/env python3
-import sys
+import os
 from argparse import ArgumentParser
-import shlex
-from mock_cli import MockCommand, ResponseDirectory
-from mock_cli.argv_conversion import argv_from_string
+
+from mock_cli import MockCommand
 
 RESPONSE_DIRECTORY_PATH = "./response-directory.json"
+RESP_DIR_ENV_NAME = "MOCK_OP_RESPONSE_DIRECTORY"
 
 
-def mock_op_arg_parser():
-    parser = ArgumentParser()
-    parser.add_argument("--global-flag", help="the global flag")
-    subparsers = parser.add_subparsers(
-        description="Available Commands", metavar="[command]", dest="command", required=True)
-    parser_get = subparsers.add_parser("get", help="the get command")
+class MockOP:
+    def __init__(self, arg_parser=None, response_directory=None):
+        if arg_parser is None:
+            arg_parser = self._mock_op_arg_parser()
+        self._arg_parser = arg_parser
+        if response_directory is None:
+            response_directory = self._get_response_directory()
+        self._response_directory = response_directory
 
-    parser_get_subparsers = parser_get.add_subparsers(
-        title="Available Commands", metavar="[command]", dest="subcommand", required=True)
+    @property
+    def response_directory_path(self):
+        return self._response_directory
 
-    parser_get_subcmd = parser_get_subparsers.add_parser(
-        "document", description="Download & print a document to standard output", help="Download a document")
-    parser_get_subcmd.add_argument(
-        "document", metavar="<document>", help="The document to get")
-    parser_get_subcmd.add_argument(
-        "--vault", help="The vault to look up and item from")
+    def _mock_op_arg_parser(self):
+        parser = ArgumentParser()
+        parser.add_argument("--global-flag", help="the global flag")
+        subparsers = parser.add_subparsers(
+            description="Available Commands", metavar="[command]", dest="command", required=True)
+        parser_get = subparsers.add_parser("get", help="the get command")
 
-    parser_get_subcmd = parser_get_subparsers.add_parser(
-        "item", description="Returns details about an item.", help="Get item details")
-    parser_get_subcmd.add_argument(
-        "item", metavar="<item>", help="The item to get")
-    parser_get_subcmd.add_argument(
-        "--fields", help="comma-separated list of fields to get about the item")
-    parser_get_subcmd.add_argument(
-        "--vault", help="The vault to look up and item from")
+        parser_get_subparsers = parser_get.add_subparsers(
+            title="Available Commands", metavar="[command]", dest="subcommand", required=True)
 
-    return parser
+        parser_get_subcmd = parser_get_subparsers.add_parser(
+            "document", description="Download & print a document to standard output", help="Download a document")
+        parser_get_subcmd.add_argument(
+            "document", metavar="<document>", help="The document to get")
+        parser_get_subcmd.add_argument(
+            "--vault", help="The vault to look up and item from")
 
+        parser_get_subcmd = parser_get_subparsers.add_parser(
+            "item", description="Returns details about an item.", help="Get item details")
+        parser_get_subcmd.add_argument(
+            "item", metavar="<item>", help="The item to get")
+        parser_get_subcmd.add_argument(
+            "--fields", help="comma-separated list of fields to get about the item")
+        parser_get_subcmd.add_argument(
+            "--vault", help="The vault to look up and item from")
 
-def main():
-    # We parse args in order to fail on args we don't understand
-    # even though we don't actually use them
-    parser = mock_op_arg_parser()
-    parser.parse_args()
+        return parser
 
-    cmd = MockCommand(RESPONSE_DIRECTORY_PATH)
-    args = sys.argv[1:]
-    exit_status = cmd.respond(args)
-    return exit_status
+    def _get_response_directory(self):
+        rdpath = os.environ.get(RESP_DIR_ENV_NAME)
+        if not rdpath:
+            rdpath = RESPONSE_DIRECTORY_PATH
+        return rdpath
 
+    def parse_args(self, argv=None):
+        if argv is None:
+            parsed = self._arg_parser.parse_args()
+        else:
+            parsed = self._arg_parser.parse_args(argv)
 
-if __name__ == "__main__":
-    exit(main())
+        return parsed
 
-
-def list_invocations_main():
-    directory = ResponseDirectory(RESPONSE_DIRECTORY_PATH)
-    commands = directory.commands
-    for cmd in commands.keys():
-        cmd_args = argv_from_string(cmd)
-        arg_string = shlex.join(cmd_args)
-
-        print(f"{arg_string}")
+    def respond(self, args):
+        cmd = MockCommand(self._response_directory)
+        exit_status = cmd.respond(args)
+        return exit_status
