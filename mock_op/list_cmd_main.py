e611c22ef477949c004efaf03e0eaf5ed6e2fe00
Zachary Cutlip <uid000@gmail.com>
2021-02-05 15:02:55 -0800

    split script entry points into separate files

---
+++ mock_op/list_cmd_main.py
@@ -0,0 +1,58 @@
+import os
+
+from argparse import ArgumentParser
+
+from mock_cli import (
+    ResponseDirectory,
+    ResponseDirectoryException
+)
+from mock_cli.argv_conversion import arg_shlex_from_string
+
+from .mock_op import MockOP
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--response-dir", help="Path to response directory JSON file")
+    parser.add_argument("--verbose", help="Include additional command response detail", action="store_true")
+
+    parsed = parser.parse_args()
+    return parsed
+
+
+def print_command_verbose(response, command_string, response_dir):
+    name = response["name"]
+    out_path = os.path.join(response_dir, name, response["stdout"])
+    err_path = os.path.join(response_dir, name, response["stderr"])
+    exit_status = response["exit_status"]
+    print(f"{command_string}")
+    print(f"\toutput: {out_path}")
+    print(f"\terror output: {err_path}")
+    print(f"\texit status: {exit_status}")
+    print("")
+
+
+def main():
+    parsed = parse_args()
+    verbose = parsed.verbose
+    if parsed.response_dir:
+        resp_dir_path = parsed.response_dir
+    else:
+        resp_dir_path = MockOP.get_response_directory()
+
+    try:
+        directory = ResponseDirectory(resp_dir_path)
+    except ResponseDirectoryException as e:
+        print(f"Error loading response directory: {e}")
+        exit(1)
+
+    print(f"Directory path: {resp_dir_path}")
+    commands = directory.commands
+    response_dir = directory.response_dir
+
+    for cmd, response in commands.items():
+        cmd_string = arg_shlex_from_string(cmd, popped_args=['op'])
+        if verbose:
+            print_command_verbose(response, cmd_string, response_dir)
+        else:
+            print(f"{cmd_string}")
---
+++ mock_op/mock_op_main.py
@@ -0,0 +1,24 @@
+import sys
+
+from mock_cli import (
+    ResponseDirectoryException,
+    ResponseLookupException
+)
+
+from .mock_op import MockOP
+
+
+def main():
+    mock_op_cmd = MockOP()
+    # We parse args in order to fail on args we don't understand
+    # even though we don't actually use them
+    mock_op_cmd.parse_args()
+
+    args = sys.argv[1:]
+    try:
+        exit_status = mock_op_cmd.respond(args)
+    except (ResponseDirectoryException, ResponseLookupException) as e:
+        print(f"Error looking up response: [{e}]", file=sys.stderr)
+        exit_status = -1
+
+    return exit_status
