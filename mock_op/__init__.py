39ce05ec8f529e68bad0b06ca1508d298e54a9d5
Zachary Cutlip <uid000@gmail.com>
2021-02-04 17:34:31 -0800

    raise NotImplementedError if pyonepassword isn't installed

--- mock_op/__init__.py
+++ mock_op/__init__.py
@@ -4,4 +4,14 @@ from .__about__ import (
     __summary__
 )
 from .mock_op import MockOP
-from .response_generator import OPResponseGenerator
+
+from . import _op
+if _op.OP:
+    from .response_generator import OPResponseGenerator
+else:
+
+    # make pyonepassword dependency a run-time error so it isn't needed if not generating responses
+    class OPResponseGenerator:
+        def __init__(self, *args, **kwargs):
+            raise NotImplementedError("pyonepassword missing, required for OPResponseGenerator")
+del _op
