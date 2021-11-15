2a518b14e60ddc8b2351ec9dbe99e516f172ba63
Zachary Cutlip <uid000@gmail.com>
2021-11-15 11:38:25 -0800

    when doing sign-in try to reuse existing session

--- mock_op/response_gen_main.py
+++ mock_op/response_gen_main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from mock_cli import CommandInvocation, ResponseDirectory
 
+from ._op import OPNotSignedInException
 from .response_generator import OPResponseGenerator
 from .response_generator_config import OPResponseGenConfig
 
@@ -21,12 +22,13 @@ def resp_gen_parse_args():
 def do_signin():
     # If you've already signed in at least once, you don't need to provide all
     # account details on future sign-ins. Just master password
-    my_password = getpass.getpass(prompt="1Password master password:\n")
-    # You may optionally provide an account shorthand if you used a custom one during initial sign-in
-    # shorthand = "arbitrary_account_shorthand"
-    # return OP(account_shorthand=shorthand, password=my_password)
-    # Or we'll try to look up account shorthand from your latest sign-in in op's config file
-    return OPResponseGenerator(password=my_password)
+    try:
+        op = OPResponseGenerator(
+            use_existing_session=True, password_prompt=False)
+    except OPNotSignedInException:
+        my_password = getpass.getpass(prompt="1Password master password:\n")
+        op = OPResponseGenerator(password=my_password)
+    return op
 
 
 def do_get_item(op: OPResponseGenerator, query_name, query_definition) -> CommandInvocation:
