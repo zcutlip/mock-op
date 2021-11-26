5184607bd3a790099393f788eb190402a17b5cdc
Zachary Cutlip <uid000@gmail.com>
2021-11-25 17:25:34 -0800

    log 'op' command invocations that are to be executed

--- mock_op/response_generator.py
+++ mock_op/response_generator.py
@@ -21,6 +21,7 @@ class OPResponseGenerator(OP):
     def get_item_generate_response(self, item_name_or_uuid, query_name, vault=None, fields=None):
         get_item_argv = self._get_item_argv(
             item_name_or_uuid, vault=vault, fields=fields)
+        self.logger.info(f"About to run: {get_item_argv.cmd_str()}")
         stdout, stderr, returncode = self._run_raw(
             get_item_argv, capture_stdout=True, ignore_error=True)
         resp_dict = self._generate_response_dict(
@@ -31,6 +32,7 @@ class OPResponseGenerator(OP):
     def get_document_generate_response(self, document_name_or_uuid: str, query_name, vault: str = None):
         get_doc_argv = self._get_document_argv(
             document_name_or_uuid, vault=vault)
+        self.logger.info(f"About to run: {get_doc_argv.cmd_str()}")
         stdout, stderr, returncode = self._run_raw(
             get_doc_argv, capture_stdout=True, ignore_error=True)
         resp_dict = self._generate_response_dict(
@@ -40,6 +42,7 @@ class OPResponseGenerator(OP):
 
     def get_vault_generate_response(self, vault_name_or_uuid, query_name):
         get_vault_argv = self._get_vault_argv(vault_name_or_uuid)
+        self.logger.info(f"About to run: {get_vault_argv.cmd_str()}")
         stdout, stderr, returncode = self._run_raw(
             get_vault_argv, capture_stdout=True, ignore_error=True)
         resp_dict = self._generate_response_dict(
@@ -49,6 +52,7 @@ class OPResponseGenerator(OP):
 
     def get_user_generate_response(self, user_name_or_uuid, query_name):
         get_user_argv = self._get_user_argv(user_name_or_uuid)
+        self.logger.info(f"About to run: {get_user_argv.cmd_str()}")
         stdout, stderr, returncode = self._run_raw(
             get_user_argv, capture_stdout=True, ignore_error=True)
         resp_dict = self._generate_response_dict(
@@ -63,6 +67,7 @@ class OPResponseGenerator(OP):
 
     def cli_version(self, query_name):
         cli_version_argv = self._cli_version_argv()
+        self.logger.info(f"About to run: {cli_version_argv.cmd_str()}")
         stdout, stderr, returncode = self._run_raw(
             cli_version_argv, capture_stdout=True, ignore_error=True)
         resp_dict = self._generate_response_dict(
@@ -71,6 +76,7 @@ class OPResponseGenerator(OP):
         return resp_dict
 
     def _resp_dict_from_argv(self, argv, query_name):
+        self.logger.info(f"About to run: {argv.cmd_str()}")
         stdout, stderr, returncode = self._run_raw(
             argv, capture_stdout=True, ignore_error=True)
         resp_dict = self._generate_response_dict(
