cbfb857b8cf83b2f5b2ea14d267b377868cd05e6
Zachary Cutlip <uid000@gmail.com>
2022-06-04 14:46:34 -0400

    import _OPArgv for special use case fo "item list templates"

--- mock_op/_op.py
+++ mock_op/_op.py
@@ -1,5 +1,7 @@
 try:
     from pyonepassword import OP, OPNotSignedInException
+    from pyonepassword._op_cli_argv import _OPArgv
 except ImportError:
+    _OPArgv = None
     OP = None
     OPNotSignedInException = None
