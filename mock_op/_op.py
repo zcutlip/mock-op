57405ac0a1e4fc771ea9a297855c470c2fab0784
Zachary Cutlip <uid000@gmail.com>
2022-06-27 18:53:10 -0700

    log import failures in _op

--- mock_op/_op.py
+++ mock_op/_op.py
@@ -1,7 +1,15 @@
+import logging
+
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+logger = logging.getLogger()
+
 try:
-    from pyonepassword import OP, OPNotSignedInException, OPSigninException
+    from pyonepassword import OP
     from pyonepassword._op_cli_argv import _OPArgv
-except ImportError:
+    from pyonepassword.api.exceptions import (OPNotSignedInException,
+                                              OPSigninException)
+except ImportError as e:
+    logger.error(f"Unable to import from pyonepassword: {e}")
     OP = None
     OPNotSignedInException = None
     OPSigninException = None
