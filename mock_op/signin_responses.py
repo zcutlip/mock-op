037f3b46c25af4f1f633bdd8feac3477ca3c22c2
Zachary Cutlip <uid000@gmail.com>
2022-09-16 18:57:32 -0700

    refactor MockOPSigninResponse to subclass MockCommand

--- mock_op/signin_responses.py
+++ mock_op/signin_responses.py
@@ -1,10 +1,11 @@
-import sys
 from datetime import datetime
 from random import choice
 from string import ascii_letters, digits
 
+from mock_cli import MockCommand, CommandResponse
 
-class MockOPSigninResponse:
+
+class MockOPSigninResponse(MockCommand):
     TOKEN_LEN = 43
     ERROR_STATUS = 1
     SUCCESS_STATUS = 0
@@ -17,31 +18,46 @@ class MockOPSigninResponse:
     )
 
     def __init__(self, shorthand, signin_success=True, raw=True):
+        changes_state = False
         if signin_success:
+            changes_state = True
             token = self._generate_token()
             if raw:
-                self._output = token
+                output = token
             else:
-                self._output = self.SUCCESS_TEMPLATE.format(
+                output = self.SUCCESS_TEMPLATE.format(
                     shorthand, token, shorthand)
-            self._error_output = None
-            self.exit_status = self.SUCCESS_STATUS
+            error_output = ''
+            exit_status = self.SUCCESS_STATUS
         else:
             _timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-            self._output = None
-            self._error_output = self.ERROR_TEMPLATE.format(_timestamp)
-            self.exit_status = self.ERROR_STATUS
+            output = ''
+            error_output = self.ERROR_TEMPLATE.format(_timestamp)
+            exit_status = self.ERROR_STATUS
+
+        output = output.encode("utf-8")
+        error_output = error_output.encode("utf-8")
+
+        resp_dict = {
+            "stdout": None,
+            "stderr": None,
+            "changes_state": changes_state,
+            "exit_status": exit_status,
+            "name": "op sign-in response"
+        }
+        super().__init__()
+        response = CommandResponse(
+            resp_dict, None, output=output, error_output=error_output)
+        self._response = response
+
+    def _get_response_directory(self, *args):
+        return None
+
+    def get_response(self, *args) -> CommandResponse:
+        return self._response
 
     def _generate_token(self):
         string_format = ascii_letters + digits
         _token = "".join(choice(string_format)
                          for x in range(0, self.TOKEN_LEN))
         return _token
-
-    def respond(self, *args):
-        if self._output is not None:
-            sys.stdout.write(self._output)
-        if self._error_output is not None:
-            sys.stderr.write(self._error_output)
-
-        return self.exit_status
