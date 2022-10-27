d0603ef8fcd60cec942a21f78ae33660afe1e35e
Zachary Cutlip <uid000@gmail.com>
2022-10-26 18:34:19 -0700

    add EXISTING_AUTH_AVAIL to _op conditional imports

--- mock_op/_op.py
+++ mock_op/_op.py
@@ -8,9 +8,11 @@ try:
     from pyonepassword._op_cli_argv import _OPArgv
     from pyonepassword.api.exceptions import (OPNotSignedInException,
                                               OPSigninException)
+    from pyonepassword.api.authentication import EXISTING_AUTH_AVAIL
 except ImportError as e:
     logger.error(f"Unable to import from pyonepassword: {e}")
     OP = None
     OPNotSignedInException = None
     OPSigninException = None
     _OPArgv = None
+    EXISTING_AUTH_AVAIL = None
