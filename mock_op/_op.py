89cc1b1de6c49af2f221871eaf60b9b5681694c3
Zachary Cutlip <uid000@gmail.com>
2023-04-19 15:59:21 -0700

    handle logging better in _op module

--- mock_op/_op.py
+++ mock_op/_op.py
@@ -1,16 +1,16 @@
 import logging
 
-logging.basicConfig(format="%(message)s", level=logging.DEBUG)
-logger = logging.getLogger()
-
 try:
     from pyonepassword import OP
+    from pyonepassword import logging as op_logging
     from pyonepassword._op_cli_argv import _OPArgv
     from pyonepassword.api.authentication import EXISTING_AUTH_AVAIL
     from pyonepassword.api.exceptions import (OPNotSignedInException,
                                               OPSigninException)
     from pyonepassword.api.object_types import OPItemList
 except ImportError as e:
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+    logger = logging.getLogger()
     logger.error(f"Unable to import from pyonepassword: {e}")
     OP = None
     OPNotSignedInException = None
@@ -18,3 +18,4 @@ except ImportError as e:
     _OPArgv = None
     EXISTING_AUTH_AVAIL = None
     OPItemList = None
+    op_logging = None
