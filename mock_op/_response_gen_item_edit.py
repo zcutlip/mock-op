90dcd008c434bf50f335b655b54c171a493c803b
Zachary Cutlip <uid000@gmail.com>
2023-08-16 13:27:41 -0700

    whitespace fix

--- mock_op/_response_gen_item_edit.py
+++ mock_op/_response_gen_item_edit.py
@@ -9,11 +9,13 @@ def item_edit_set_password(op: OPResponseGenerator,
                            query_definition,
                            item_id,
                            vault) -> CommandInvocation:
+
     password = query_definition["password"]
     field_label = query_definition["field_label"]
     section_label = query_definition.get("section_label", None)
     expected_return = query_definition.get("expected-return", 0)
     changes_state = query_definition.get("changes_state", False)
+
     invocation = op.item_edit_set_password_generate_response(item_id,
                                                              query_name,
                                                              password,
