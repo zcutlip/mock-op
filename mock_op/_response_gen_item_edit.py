3701bb26189e11d8da6f8f146ef0965aaad70e44
Zachary Cutlip <uid000@gmail.com>
2023-09-25 19:35:37 -0700

    item_edit_set_text_field() set field type to text

--- mock_op/_response_gen_item_edit.py
+++ mock_op/_response_gen_item_edit.py
@@ -39,7 +39,7 @@ def item_edit_set_text_field(op: OPResponseGenerator,
     section_label = query_definition.get("section-label", None)
     expected_return = query_definition.get("expected-return", 0)
     changes_state = query_definition.get("changes-state", False)
-    field_type = OPFieldTypeEnum.PASSWORD
+    field_type = OPFieldTypeEnum.TEXT
     invocation = op._item_edit_set_field_value(item_id,
                                                query_name,
                                                field_type,
